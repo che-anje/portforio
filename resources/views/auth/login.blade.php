6467a36d93c4c8f94e6d1d34503639891ccc02d2
che-anje <anje19960225@gmail.com>
2020-07-14 01:38:58 +0900

    200714

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,35 +38,7 @@
         <button type="submit" name="commit" class="btn btn-primary btn-primary--grad 
         mx-auto mb-1 mt-3 text-fw-bold">{{ __('ログインする') }}</button>
     </form>   
-        <hr class="mb-1">
 
-<!--  <form class="mb-1" id="new_user" method="POST" action="{{ route('login') }}" accept-charset="UTF-8">
-    @csrf
-       
-        <button type="submit" name="commit" class="btn btn-primary btn-primary--grad 
-        mx-auto mb-1 mt-3 text-fw-bold">{{ __('かんたんログイン') }}</button>
-        <input type="hidden" name="authenticity_token" value="TSFaCIy40j3lniqREkjSeJTcuZIm2lJP8Fzg0ZSRZH+4a3YcWQaDYVBa3eZ/DQuxEZ7raryP1kzFez8z2AKJcg==" />
-        <input id="invite_activation_key" name="user[invite_activation_key]" type="hidden" value="" />
-
-       
-        <input autofocus="autofocus" id="email" type="hideen" name="email" placeholder="メールアドレス" 
-        class="form-control mb-0 rounded-0 border-0 pt-4_5 pb-4_5 @error('email') is-invalid @enderror" value="{{ old('email') }}" required autocomplete="email">
-
-        @error('email')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-        @enderror
-        
-        <input autocomplete="off" id="password" name="password" placeholder="パスワード"
-        type="hidden" class="form-control mb-0 rounded-0 border-0 pt-4_5 pb-4_5 @error('password') is-invalid @enderror" required autocomplete="new-password">
-        @error('password')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-        @enderror
-    </form>
--->
 </div>
 <div class="container col-md-8 col-lg-6">
     <hr class="mb-3_5">
