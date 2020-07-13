e9a72cb50391b545bd17ecebcaca59e095037a0d
che-anje <anje19960225@gmail.com>
2020-07-13 20:41:50 +0900

    200710

--- app/Http/Requests/EditProfileRequest.php
+++ app/Http/Requests/EditProfileRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Hash;
 
 class EditProfileRequest extends FormRequest
 {
