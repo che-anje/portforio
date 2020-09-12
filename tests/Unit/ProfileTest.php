55f151d8d21f6643dbc53c406ed5e0094cde8bbf
che-anje <anje19960225@gmail.com>
2020-09-13 00:16:26 +0900

    create UnitTest

---
+++ tests/Unit/ProfileTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ProfileTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testGetGenderDescription_【条件】_【期待する結果】() {
+    }
+
+    public function testGetUsersImagePath_【条件】_【期待する結果】() {
+    }
+}
