593e8af6a046438948589c3aed6c88ec4fb739d0
che-anje <anje19960225@gmail.com>
2020-09-13 18:27:50 +0900

    作業完了

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testUpdateProfile_【条件】_【期待する結果】() {
+    }
+}
