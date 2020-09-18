98ea65763afb5eb93873a1ca8d0c3efd4c3b4466
che-anje <anje19960225@gmail.com>
2020-09-18 14:47:24 +0000

    add GenderTest

---
+++ tests/Unit/GenderTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class GenderTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testGenderDescription() {
+
+    }
+
+    public function testGenderValue() {
+        
+    }
+}
