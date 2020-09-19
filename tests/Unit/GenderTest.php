4269008ca4857e5494348aed9f0b3799f6829298
che-anje <anje19960225@gmail.com>
2020-09-19 08:59:58 +0000

    edit GenderTest

--- tests/Unit/GenderTest.php
+++ tests/Unit/GenderTest.php
@@ -3,9 +3,15 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use App\Enums\Gender;
 
 class GenderTest extends TestCase
 {
+    protected $gender;
+    protected function setUp(): void
+    {
+        $this->array = array("未設定","男性","女性");
+    }
     /**
      * A basic unit test example.
      *
@@ -17,10 +23,16 @@ class GenderTest extends TestCase
     }
 
     public function testGenderDescription() {
-
+        foreach($this->array as $index => $value) {
+            $description = Gender::getDescription($index);
+            $this->assertSame($value, $description);
+        }
     }
 
     public function testGenderValue() {
-        
+        foreach($this->array as $index => $value) {
+            $genderValue = Gender::getValue($value);
+            $this->assertSame($index, $genderValue);
+        }
     }
 }
